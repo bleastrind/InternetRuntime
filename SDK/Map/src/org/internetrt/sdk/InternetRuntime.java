91f90daf86415d08fb677481340321dd715f19ca
Wang fei <wangfei.wg@gmail.com>
2012-06-20 20:13:26 +0800

    SDK

---
+++ SDK/Map/src/org/internetrt/sdk/InternetRuntime.java
@@ -0,0 +1,14 @@
+package org.internetrt.sdk;
+
+import java.util.Map;
+
+public class InternetRuntime {
+
+	public void send(String from, String signalName, Map<String,String> sourceMap){
+		//server 请求 拿到xml httpClient 认证 李毅坚 
+		//解析xml
+		//sourceMap to targetMap
+		//填充 装配个请求
+		//发出请求
+	}
+}
